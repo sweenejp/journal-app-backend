d8976d1f77e90480a7a1dd4b0f78f9eba383525c
sweenejp <sweenejp@gmail.com>
2021-11-17 22:10:35 -0600

    add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import cors from 'cors';
 import express from 'express';
 import entriesRouter from './routes/entries.js';
 import { connectDB } from './db/connect.js';
@@ -10,6 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
 
