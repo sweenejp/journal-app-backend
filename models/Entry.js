bbfbc23e5e3fcd5178d9ff3af556e5df0492c810
sweenejp <sweenejp@gmail.com>
2021-11-21 22:44:40 -0600

    add dates

--- models/Entry.js
+++ models/Entry.js
@@ -8,6 +8,8 @@ const EntrySchema = new mongoose.Schema({
   },
   body: { type: String, default: "There's nothing here :(" },
   tags: { type: Array, default: [] },
+  dateCreated: { type: Date, default: new Date() },
+  dateUpdated: { type: Date, default: new Date() },
 });
 
 export default mongoose.model('Entry', EntrySchema);
