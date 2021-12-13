1a4ebe171132935e767c1bbc7261f8110619a959
sweenejp <sweenejp@gmail.com>
2021-12-13 15:13:43 -0600

    add security middleware

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 import 'express-async-errors';
 import cors from 'cors';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import rateLimiter from 'express-rate-limit';
 import connectDB from './db/connect.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
@@ -12,15 +15,24 @@ const app = express();
 // routers
 import entriesRouter from './routes/entries.js';
 import authRouter from './routes/auth.js';
-import authMiddleware from './middleware/authentication.js';
+
+import authenticateUser from './middleware/authentication.js';
 
 // middleware
-app.use(cors());
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100,
+  })
+);
 app.use(express.json());
-app.use(express.static('./public'));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
-app.use('/api/v1/entries', authMiddleware, entriesRouter);
+app.use('/api/v1/entries', authenticateUser, entriesRouter);
 app.use('/api/v1/auth', authRouter);
 
 // error handler middleware
